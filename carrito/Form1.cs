3d1e51c8bf2fd86239e810eae5af0aa52e05a9c3
unknown <nicolaszanetta@gmail.com>
2023-09-02 20:20:40 -0400

    comentarios

--- carrito/Form1.cs
+++ carrito/Form1.cs
@@ -8,66 +8,79 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Define el espacio de nombres y la clase del formulario.
 namespace carrito
 {
     public partial class Form1 : Form
     {
+        // Constructor del formulario.
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Método que se ejecuta al cargar el formulario.
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Agrega elementos al ComboBox (cboProductos) cuando el formulario se carga.
             cboProductos.Items.Add("Pantalon");
             cboProductos.Items.Add("Camisa");
             cboProductos.Items.Add("Zapatos");
         }
 
+        // Evento que se dispara al hacer clic en el botón "Agregar".
         private void btnAgregar_Click(object sender, EventArgs e)
         {
+            // Obtiene el producto seleccionado en el ComboBox.
             string productoSeleccionado = cboProductos.SelectedItem.ToString();
-            listCarritto.Items.Add(productoSeleccionado);
 
+            // Agrega el producto seleccionado a la lista (listCarritto).
+            listCarritto.Items.Add(productoSeleccionado);
         }
 
+        // Evento que se dispara al hacer clic en el botón "Calcular".
         private void btnCalcuclar_Click(object sender, EventArgs e)
         {
             double precioFinal = 0;
 
+            // Obtiene el número total de productos en la lista.
             int TotalProductos = listCarritto.Items.Count;
 
-            for (int i = 0; i < TotalProductos; i++) {
+            // Recorre la lista de productos.
+            for (int i = 0; i < TotalProductos; i++)
+            {
                 string productoElegido = listCarritto.Items[i].ToString();
-                if (productoElegido == "Pantalon") {
 
+                // Calcula el precio final en función del producto seleccionado.
+                if (productoElegido == "Pantalon")
+                {
                     precioFinal += 20;
                 }
                 else if (productoElegido == "Camisa")
                 {
-
                     precioFinal += 35;
                 }
                 if (productoElegido == "Zapatos")
                 {
-
                     precioFinal += 10;
                 }
-
             }
 
+            // Verifica si se ingresó un código de descuento.
             if (codigoDescuento.Text == "NIKE")
             {
+                // Aplica un descuento del 10% si el código es "NIKE".
                 precioFinal = precioFinal * 0.9;
             }
 
+            // Muestra un mensaje con el precio final.
             MessageBox.Show("El precio final es: $" + precioFinal);
         }
 
+        // Evento del label (no contiene código en este ejemplo).
         private void label1_Click(object sender, EventArgs e)
         {
-
+            // Puede dejar en blanco o agregar código adicional aquí.
         }
     }
-    }
-
+}
